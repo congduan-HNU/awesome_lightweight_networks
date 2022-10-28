ebc86b50b727248e5ff7495edde3e366a6373ff5
congduan-HNU <653875986@qq.com>
2022-10-28 21:28:30 +0800

    Update mobile_vit.py

--- light_cnns/Transformer/mobile_vit.py
+++ light_cnns/Transformer/mobile_vit.py
@@ -226,19 +226,19 @@ class MobileViT(nn.Module):
         return x
 
 
-def mobilevit_xxs():
+def mobilevit_xxs(num_classes):
     dims = [64, 80, 96]
     channels = [16, 16, 24, 24, 48, 48, 64, 64, 80, 80, 320]
-    return MobileViT((256, 256), dims, channels, num_classes=1000, expansion=2)
+    return MobileViT((256, 256), dims, channels, num_classes=num_classes, expansion=2)
 
 
-def mobilevit_xs():
+def mobilevit_xs(num_classes):
     dims = [96, 120, 144]
     channels = [16, 32, 48, 48, 64, 64, 80, 80, 96, 96, 384]
-    return MobileViT((256, 256), dims, channels, num_classes=1000)
+    return MobileViT((256, 256), dims, channels, num_classes=num_classes)
 
 
-def mobilevit_s():
+def mobilevit_s(num_classes):
     dims = [144, 192, 240]
     channels = [16, 32, 64, 64, 96, 96, 128, 128, 160, 160, 640]
-    return MobileViT((256, 256), dims, channels, num_classes=1000)
+    return MobileViT((256, 256), dims, channels, num_classes=num_classes)
